53807a8a42c44c8f78d3ec34e0369a51297b66ec
Ahron Henrique Helfenstein <ahron.helfenstein@gmail.com>
2018-04-10 22:29:27 -0300

    Correção inicial

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div id="app">
         <main>
-          
+          teste
         </main>
       </div>
     );
